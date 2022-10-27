3816301b6cfd1dcf36f1c24cc501ba4fae5c3924
Ab3id <abeidtwo@gmail.com>
2022-10-27 14:20:47 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$my_details = array();
+$my_details['slackUsername'] = 'Ab3id';
+$my_details['backend'] = true;
+$my_details['age'] = 23;
+$my_details['bio'] = 'Software Engineer passionate automating and solving complex and simple tasks through technology.';
+
+echo json_encode($my_details);
+
+?>
